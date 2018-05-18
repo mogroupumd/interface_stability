828d56025fc4093110e6e1389e10e8226be42463
boatpuma <yizhou.zhu@gmail.com>
2018-05-18 14:53:38 -0400

    sync

--- interface_stability/tests/test_interface_stability.py
+++ interface_stability/tests/test_interface_stability.py
@@ -1,8 +1,13 @@
 import unittest
 from pymatgen import MPRester, SETTINGS
 
-@unittest.skipIf(not SETTINGS.get("PMG_MAPI_KEY"), "PMG_MAPI_KEY environment variable not set.")
+#@unittest.skipIf(not SETTINGS.get("PMG_MAPI_KEY"), "PMG_MAPI_KEY environment variable not set.")
+
 class InterfaceStabilityTest(unittest.TestCase):
+    def test_get_api_key(self):
+        self.assertTrue(SETTINGS.get("PMG_MAPI_KEY"))
+
+
     def test_get_structure_by_material_id(self):
         with MPRester() as m:
             s1 = m.get_structure_by_material_id("mp-1")
